77a4c2df72b92376b40d822e3a473090c6a60988
Amber <amberlagrange99@gmail.com>
2023-09-24 16:29:53 +0000

    Added standard streams and Files for now.

--- include/stdio.h
+++ include/stdio.h
@@ -1,11 +1,20 @@
 #ifndef STDIO_H
 #define STDIO_H
 
-extern const int stdin;
-extern const int stdout;
-extern const int stderr;
+typedef struct __FILE_IO {
+    // TODO: actually implement this properly
+    int fd;
+} FILE;
 
-int fprintf(int fd, const char* fmt, ...);
+#define FOPEN_MAX 16
+
+extern FILE *_Files[FOPEN_MAX];
+
+#define stdin  _Files[0]
+#define stdout _Files[1]
+#define stderr _Files[2]
+
+int fprintf(FILE *file, const char* fmt, ...);
 int printf(const char *fmt, ...);
 
 #endif // STDIO_H
--- stdlib/stdio.c
+++ stdlib/stdio.c
@@ -3,14 +3,16 @@
 #include "../include/stdlib.h"
 #include "../include/string.h"
 
-const int stdin  = 0;
-const int stdout = 1;
-const int stderr = 2;
-
-int fprintf(int fd, const char *fmt, ...) {
-    return write(fd, fmt, strlen(fmt));
+int fprintf(FILE *file, const char *fmt, ...) {
+    return write(file->fd, fmt, strlen(fmt));
 }
 
 int printf(const char *fmt, ...) {
     return fprintf(stdout, fmt);
 }
+
+static FILE _STDIN  = {0};
+static FILE _STDOUT = {1};
+static FILE _STDERR = {2};
+
+FILE *_Files[FOPEN_MAX] = { &_STDIN, &_STDOUT, &_STDERR };
