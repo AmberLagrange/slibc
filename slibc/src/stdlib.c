ece8f256095a59168b65eb92c574110b8070a8f8
Amber <amberlagrange99@gmail.com>
2023-10-04 07:17:11 +0000

    malloc is now 16 byte aligned (for now).

--- slibc/src/stdlib.c
+++ slibc/src/stdlib.c
@@ -4,21 +4,23 @@
 
 #include "../include/mman.h"
 
+#define MAX_ALIGNMENT 16
+
 void *malloc(unsigned long size) {
 
-    unsigned long *ptr;
+    unsigned char *ptr;
     unsigned long new_size;
 
     if (!size) {
         return NULL;
     }
 
-    new_size = size + sizeof(size);
+    new_size = size + MAX_ALIGNMENT;
 
     ptr = mmap(0, new_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     *ptr = new_size;
 
-    return ptr + sizeof(size);
+    return ptr + MAX_ALIGNMENT;
 }
 
 void free(void *ptr) {
@@ -30,7 +32,7 @@ void free(void *ptr) {
         return;
     }
 
-    size = *((unsigned long *)ptr - sizeof(size)); /* Get the size stored before the pointer */
+    size = *((unsigned char *)ptr - MAX_ALIGNMENT); /* Get the size stored before the pointer */
     munmap(ptr, size);
 
     return;
--- test/src/main.c
+++ test/src/main.c
@@ -4,10 +4,17 @@
 
 int main(void) {
 
-    int i;
-    i = (int)(sizeof(unsigned long));
+    int *ptr_1;
+    int *ptr_2;
 
-    printf("sizeof(unsigned long): %d\n", i);
+    ptr_1 = malloc(sizeof(int));
+    ptr_2 = malloc(sizeof(int));
+
+    printf("ptr_1: %p\n", (void*)ptr_1);
+    printf("ptr_2: %p\n", (void*)ptr_2);
+
+    free(ptr_2);
+    free(ptr_1);
 
     return 0;
 }
