3d8a2c27d6864e9e7af33ed69c8a84119c06cc64
Amber <amberlagrange99@gmail.com>
2023-10-16 19:51:12 +0000

    Fixerd example program to appease valgrind.

--- example/src/main.c
+++ example/src/main.c
@@ -9,13 +9,13 @@ int main(void) {
     ptr_1 = malloc(sizeof(int) * 1);
     *ptr_1 = 42;
     printf("Before realloc: pointer: %p, value: %d\n", (void *)ptr_1, *ptr_1);
-    ptr_2 = realloc(ptr_1, 2);
+    ptr_2 = realloc(ptr_1, sizeof(int) * 2);
     ptr_2[0] = 0x42;
     ptr_2[1] = 0x1337;
     printf("After realloc: pointer: %p, values: 0x%x, 0x%x\n", (void *)ptr_2, ptr_2[0], ptr_2[1]);
     free(ptr_2);
-    printf("This might segfault: 0x%x\n", *ptr_1);
-    printf("This might also segfault: %d\n", ptr_2[0]);
+    /* printf("This might segfault: 0x%x\n", *ptr_1); */
+    /* printf("This might also segfault: %d\n", ptr_2[0]); */
 
     return 0;
 }
