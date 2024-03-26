5e885bbf7fc9218a2bbe4fdf426f9660bd398406
Amber <amberlagrange99@gmail.com>
2024-03-25 23:49:21 -0400

    Added example code to doxygen.
    Hopefully added source code to doxygen for .c files too.

--- example/src/main.c
+++ example/src/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#include <ctype.h>
 
+/**
+ * @example 
+ */
 int main(void) {
 
-    int n = 123;
+    int num_read = 0;
 
     printf("Hello, %s\n", "World!");
-    printf("This should have padding (it doesn't): %5d\n", 5);
-    printf("123456789%n\n", &n);
-    printf("Wrote %d characters!\n", n);
+    printf("This should have padding (it doesn't): %5d\n", 0);
+    printf("123456789%n\n", &num_read);
+    printf("Wrote %d characters!\n", num_read);
 
     return 0;
 }
