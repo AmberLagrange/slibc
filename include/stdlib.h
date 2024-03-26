84d2305989f7e460671ae1f901876cc52835d1a5
Amber <amberlagrange99@gmail.com>
2024-03-25 21:16:42 -0400

    Added more documentation.
    This is mainly to test the GitHub Action of updating the server.

--- include/stdlib.h
+++ include/stdlib.h
@@ -3,19 +3,95 @@
 
 #define NULL ((void*)0)
 
-double  atof(const char *str);
-int     atoi(const char *str);
-long    atol(const char *str);
+/**
+ * @brief           Parses a double from a given string.
+ * 
+ * @param str       The string to parse the double from.
+ * @return double   The double that was parsed.
+ */
+double atof(const char *str);
 
-long          strtol(const char  *str, char **str_end, int radix);
+/**
+ * @brief           Parses an integer from a given string.
+ * 
+ * @param str       The string to parse the integer from.
+ * @return int      The integer that was parsed.
+ */
+int atoi(const char *str);
+
+/**
+ * @brief           Parses a long integer from a given string.
+ * 
+ * @param str       The string to parse the long integer from.
+ * @return long     The long integer that was parsed.
+ */
+long atol(const char *str);
+
+/**
+ * @brief           Parses a long integer from a given string in a chosen base.
+ * 
+ * @param str       The string to parse the long integer from.
+ * @param str_end   The first character after the number parsed from str.
+ * @param radix     The base to parse the character in.
+ * @return long     The long integer that was parsed.
+ */
+long strtol(const char *str, char **str_end, int radix);
+
+/**
+ * @brief           Parses an unsigned long integer from a given string in a chosen base.
+ * 
+ * @param str       The string to parse the unsigned long integer from.
+ * @param str_end   The first character after the number parsed from str.
+ * @param radix     The base to parse the character in.
+ * @return long     The unsigned long integer that was parsed.
+ */
 unsigned long strtoul(const char *str, char **str_end, int radix);
-double        strtod(const char  *str, char **str_end);
 
-void *malloc (           unsigned long size);
+/**
+ * @brief           Parses a double from a given string in a chosen base.
+ * 
+ * @param str       The string to parse the double from.
+ * @param str_end   The first character after the number parsed from str.
+ * @param radix     The base to parse the character in.
+ * @return long     The double that was parsed.
+ */
+double strtod(const char *str, char **str_end);
+
+/**
+ * @brief           Creates memory region of a given size and returns it to the caller.
+ * 
+ * @param size      The size of the requested memory.
+ * @return void*    The address of the memory that was created.
+ */
+void *malloc (unsigned long size);
+
+/**
+ * @brief           Creates a new memory region of a given size and copies the data over to this new region.
+                    This also frees the old memory.
+ * 
+ * @param ptr       The pointer of the original memory region.
+ * @param size      The requested size of the new memory region.
+ * @return void*    The address of the new memory region.
+ */
 void *realloc(void *ptr, unsigned long size);
-void  free   (void *ptr);
 
+/**
+ * @brief           Returns memory that was created used malloc.
+ * 
+ * @param ptr       The address of the memory to be returned.
+ */
+void free (void *ptr);
+
+/**
+ * @brief           Abort the running process. Internally, calls raise(SIG_ABORT) 
+ * 
+ */
 __attribute__((noreturn)) void abort(void);
+
+/**
+ * @brief           Exits the running process by calling the SYS_EXIT syscall and returning the status to the parent process.
+ * 
+ */
 __attribute__((noreturn)) void exit(int status);
 
 #endif /* STDLIB_H */
