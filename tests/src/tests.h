fc301e580bcd4b42e4428214650b6c83bb6493ea
Amber <amberlagrange99@gmail.com>
2024-04-21 01:02:55 -0400

    The test macros now call return macros internally.

--- tests/src/tests.h
+++ tests/src/tests.h
@@ -5,57 +5,51 @@
 #define GREEN   "\033[32m"
 #define YELLOW  "\033[33m"
 
+#define PASSED   do { printf("%sPASSED%s\n",    GREEN, RESET); return 0; } while (0)
+#define FAILED   do { printf("%sFAILED%s\n",      RED, RESET); return 1; } while (0)
+#define DISABLED do { printf("%sDISABLED%s\n", YELLOW, RESET); return 0; } while (0)
+
 #define ASSERT_EQ(lhs, rhs)                     \
     do {                                        \
         if((lhs) != (rhs)) {                    \
-            printf("%sFAILED%s\n", RED, RESET); \
-            return 1;                           \
+            FAILED;                             \
         }                                       \
     } while(0)
 
 #define ASSERT_NE(lhs, rhs)                     \
     do {                                        \
         if((lhs) == (rhs)) {                    \
-            printf("%sFAILED%s\n", RED, RESET); \
-            return 1;                           \
+            FAILED;                             \
         }                                       \
     } while(0)
 
 #define ASSERT_LT(lhs, rhs)                     \
     do {                                        \
         if((lhs) >= (rhs)) {                    \
-            printf("%sFAILED%s\n", RED, RESET); \
-            return 1;                           \
+            FAILED;                             \
         }                                       \
     } while(0)
 
 #define ASSERT_LE(lhs, rhs)                     \
     do {                                        \
         if((lhs) > (rhs)) {                     \
-            printf("%sFAILED%s\n", RED, RESET); \
-            return 1;                           \
+            FAILED;                             \
         }                                       \
     } while(0)
 
 #define ASSERT_GT(lhs, rhs)                     \
     do {                                        \
         if((lhs) <= (rhs)) {                    \
-            printf("%sFAILED%s\n", RED, RESET); \
-            return 1;                           \
+            FAILED;                             \
         }                                       \
     } while(0)
 
 #define ASSERT_GE(lhs, rhs)                     \
     do {                                        \
         if((lhs) < (rhs)) {                     \
-            printf("%sFAILED%s\n", RED, RESET); \
-            return 1;                           \
+            FAILED;                             \
         }                                       \
     } while(0)
-
-#define PASSED   do { printf("%sPASSED%s\n",    GREEN, RESET); return 0; } while (0)
-#define FAILED   do { printf("%sFAILED%s\n",      RED, RESET); return 1; } while (0)
-#define DISABLED do { printf("%sDISABLED%s\n", YELLOW, RESET); return 0; } while (0)
 
 int run_stdlib_tests(void);
 int run_ctype_tests(void);
